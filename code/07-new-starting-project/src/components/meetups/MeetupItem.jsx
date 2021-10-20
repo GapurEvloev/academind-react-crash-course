a53f9d441fccfe679ed87c9bf27930df14dabeb6
Gapur <kaloycant@gmail.com>
2021-10-20 19:32:03 +0300

    2:11

--- code/07-new-starting-project/src/components/meetups/MeetupItem.jsx
+++ code/07-new-starting-project/src/components/meetups/MeetupItem.jsx
@@ -1,5 +1,19 @@
-function MeetupItem() {
-    return <li></li>
+import classes from './MeetupItem.module.css'
+
+function MeetupItem(props) {
+    return <li className={classes.item}>
+        <div className={classes.image}>
+            <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+            <h3>{props.title}</h3>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+            <button>To favorites</button>
+        </div>
+    </li>
 }
 
 export default MeetupItem;
