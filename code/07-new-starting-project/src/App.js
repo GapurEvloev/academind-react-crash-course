9c86c88cdb3a796f162163ca299cdde2e3df6617
Gapur <kaloycant@gmail.com>
2021-10-21 21:13:22 +0300

    3:14

--- code/07-new-starting-project/src/App.js
+++ code/07-new-starting-project/src/App.js
@@ -1,25 +1,26 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import AllMeetupsPage from './pages/AllMeetup';
-import NewMeetupsPage from './pages/NewMeetup';
-import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import AllMeetupsPage from "./pages/AllMeetup";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return <div>
-    <MainNavigation />
-    <Switch>
-      <Route path="/" exact>
-        <AllMeetupsPage />
-      </Route>
-      <Route path="/new-meetup">
-        <NewMeetupsPage />
-      </Route>
-      <Route path="/favorites">
-        <FavoritesPage  />
-      </Route>
-    </Switch>
-  </div>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
--- code/07-new-starting-project/src/pages/NewMeetup.jsx
+++ code/07-new-starting-project/src/pages/NewMeetup.jsx
@@ -1,5 +1,31 @@
+import { useHistory } from 'react-router-dom'
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-  return <div>New Meetup</div>;
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-6e4ad-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <section className={""}>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
